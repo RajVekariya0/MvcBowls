0f1c6c1584e32b4cae4f8903259b72a8811af655
Raj Mukeshbhai Vekariya <W0802730@myscc.ca>
2023-02-16 13:58:21 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcBowls.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
