c0aace5e4a46cf8ca8a61a6b173a71f9adf0b5aa
Raj Mukeshbhai Vekariya <W0802730@myscc.ca>
2023-02-16 15:35:22 -0500

    Added an controlled  called BowlsController.cs

---
+++ Controllers/BowlsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBowls.Controllers
+{
+    public class BowlsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
