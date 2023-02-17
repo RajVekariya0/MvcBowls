2bd797f4479479e1ed1f50f7f7bfef7133cc1b2a
Raj Mukeshbhai Vekariya <W0802730@myscc.ca>
2023-02-17 15:29:00 -0500

    ADDED BOWL.CS FILE TO MODEL AND ADDED 6 PROPERTUIES TO IT

---
+++ Models/Bowl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBowls.Models
+{
+    public class Bowl
+    {
+        public int Id { get; set; }
+        public string VarietyOfBowls { get; set; }
+        public string Color { get; set; }
+
+        public string Material { get; set; }
+        public decimal Uses { get; set; }
+        public decimal Price { get; set; }
+        public decimal Rating { get; set; }
+
+    }
+}
